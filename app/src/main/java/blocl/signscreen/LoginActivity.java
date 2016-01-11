2498c16a749c0dcbd0bed8035deb1edf35b8e733
Justin Leonard <jhuskies@hotmail.com>
2016-01-11 09:43:04 -0800

    Initial files

--- app/src/main/java/blocl/signscreen/LoginActivity.java
+++ app/src/main/java/blocl/signscreen/LoginActivity.java
@@ -102,7 +102,7 @@ public class LoginActivity extends PlusBaseActivity implements LoaderCallbacks<C
             }
         });
 
-        Button mEmailSignInButton = (Button) findViewById(R.id.email_sign_in_button);
+        Button mEmailSignInButton = (Button) findViewById(R.id.plus_sign_in_button);
         mEmailSignInButton.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View view) {
